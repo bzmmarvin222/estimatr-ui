f476e535e3c7de0b571ca4f624229fe40faa893a
bzmmarvin222 <bzmmarvin222@gmail.com>
2018-12-11 23:58:25 +0100

    topics can now be renamed and single estimates can be added

--- src/app/syncable/estimation/topic/topic.component.ts
+++ src/app/syncable/estimation/topic/topic.component.ts
@@ -1,7 +1,11 @@
 import {Component, HostBinding, Input, OnInit} from '@angular/core';
 import {Operation, SyncableTree} from 'sync_ot';
-import {EstimationLeaf, EstimationNode} from '../../shared/estimation-node';
+import {EstimationDescription, EstimationLeaf, EstimationNode} from '../../shared/estimation-node';
 import {SyncableService} from '../../services/syncable.service';
+import {PromptDialog} from "../../../shared/models/dialog";
+import {PromptDialogComponent} from "../../../shared/prompt-dialog/prompt-dialog.component";
+import {take} from "rxjs/operators";
+import {MatDialog} from "@angular/material";
 
 @Component({
   selector: 'etmr-topic',
@@ -12,7 +16,8 @@ export class TopicComponent implements OnInit {
   @HostBinding('class.accordion')
   @Input() public topicNode: SyncableTree<EstimationNode>;
 
-  constructor(private _sync: SyncableService) { }
+  constructor(private _sync: SyncableService,
+              private _dialog: MatDialog) { }
 
   ngOnInit() {
   }
@@ -20,6 +25,32 @@ export class TopicComponent implements OnInit {
   public addSingleEstimate(): void {
     const estimate: EstimationLeaf = {effortInManDays: 0, taskDescription: ''};
     const operation: Operation = this.topicNode.createChildAppend(estimate);
+    this._sync.sr.queueOperation(operation);
+  }
+
+  public rename(): void {
+    const beforeDialog: string = this.topicNode.data as EstimationDescription;
+    const data: PromptDialog = {header: 'Package name', explanation: 'Please enter a name for this package.', placeholder: 'Name', promptData: this.topicNode.data as EstimationDescription};
+    this._dialog.open(PromptDialogComponent, {
+      data: data
+    }).afterClosed()
+      .pipe(take(1))
+      .subscribe((val: any | undefined) => {
+        const alreadyChanged = this.topicNode.data !== beforeDialog;
+        const changedToSameValue = this.topicNode.data == val;
+
+        if (val && typeof val === 'string' && !alreadyChanged) {
+          this.queueRenameOperation(val);
+        } else if (val && !changedToSameValue) {
+          //TODO: make more beautiful
+          alert('already changed by someone else...');
+        }
+
+      });
+  }
+
+  private queueRenameOperation(newName: string): void {
+    const operation: Operation = this.topicNode.createReplacement(newName);
     this._sync.sr.queueOperation(operation);
   }
 }
