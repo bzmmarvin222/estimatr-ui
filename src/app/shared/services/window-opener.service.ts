26d7e5ce1601ddcb0c15cccf61ef3438d23cc332
bzmmarvin222 <bzmmarvin222@gmail.com>
2019-01-26 00:44:56 +0100

    fix extremely oversized windows with a max. zoom of 1.2 used for popups

--- src/app/shared/services/window-opener.service.ts
+++ src/app/shared/services/window-opener.service.ts
@@ -40,7 +40,7 @@ export class WindowOpenerService {
     const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
     const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
-    const systemZoom = width / window.screen.availWidth;
+    const systemZoom = Math.max(width / window.screen.availWidth, 1.2);
     const left = (width - w) / 2 / systemZoom + dualScreenLeft;
     const top = (height - h) / 2 / systemZoom + dualScreenTop;
     const features: string = `scrollbars=no, width=${w / systemZoom}, height=${h / systemZoom}, top=${top}, left=${left}`;
