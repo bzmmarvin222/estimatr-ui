cf53986598c7fb626727e5589b2cf1cf25492e34
bzmmarvin222 <bzmmarvin222@gmail.com>
2019-03-16 20:57:06 +0100

    extract estimation tree effort calculation to util

--- src/app/shared/util/estimation-tree.util.ts
+++ src/app/shared/util/estimation-tree.util.ts
@@ -1,16 +1,17 @@
-import {EstimationLeaf, EstimationNode} from '../../estimatr-common/lib/estimation/estimation';
+import {EstimationLeaf, EstimationNode, EstimationRoot} from '../../estimatr-common/lib/estimation/estimation';
 import {RiskFactors} from '../../estimatr-common/lib/estimation/risk';
 import {SyncableTree} from 'sync_ot';
 
-/**export const calculateEffort = (node: SyncableTree<EstimationNode>, riskFactors: RiskFactors): number => {
-  return node.children.reduce((prev: number, curr: SyncableTree<EstimationNode>) => prev + this.mapWorkItemPackage(curr, riskFactors), 0);
+export const calculateEffort = (node: SyncableTree<EstimationNode>): number => {
+  const riskFactors = (node.data as EstimationRoot).riskFactors;
+  return node.children.reduce((prev: number, curr: SyncableTree<EstimationNode>) => prev + calculateItemPackageEffort(curr, riskFactors), 0);
 };
 
-private mapWorkItemPackage(pck: SyncableTree<EstimationNode>, factors: RiskFactors): number {
-  const items = pck.children as SyncableTree<EstimationLeaf>[];
-  return items.reduce((prev: number, curr: SyncableTree<EstimationLeaf>) => prev + this.mapItem(curr.data, factors), 0);
-}
+const calculateItemPackageEffort = (node: SyncableTree<EstimationNode>, factors: RiskFactors): number => {
+  const items = node.children as SyncableTree<EstimationLeaf>[];
+  return items.reduce((prev: number, curr: SyncableTree<EstimationLeaf>) => prev + calculateLeafEffort(curr.data, factors), 0);
+};
 
-private mapItem(leaf: EstimationLeaf, factors: RiskFactors): number {
+const calculateLeafEffort = (leaf: EstimationLeaf, factors: RiskFactors): number => {
   return +factors[leaf.risk] * leaf.effortInManDays;
-}**/
+};
--- src/app/syncable/pipes/estimation-tree-sum.pipe.ts
+++ src/app/syncable/pipes/estimation-tree-sum.pipe.ts
@@ -1,7 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {SyncableTree} from 'sync_ot';
-import {EstimationLeaf, EstimationNode, EstimationRoot} from '../../estimatr-common/lib/estimation/estimation';
-import {RiskFactors} from '../../estimatr-common/lib/estimation/risk';
+import {EstimationNode} from '../../estimatr-common/lib/estimation/estimation';
+import {calculateEffort} from '../../shared/util/estimation-tree.util';
 
 @Pipe({
   name: 'estimationTreeSum',
@@ -10,17 +10,7 @@ import {RiskFactors} from '../../estimatr-common/lib/estimation/risk';
 export class EstimationTreeSumPipe implements PipeTransform {
 
   transform(root: SyncableTree<EstimationNode>): number {
-    const riskFactors = (<EstimationRoot>root.data).riskFactors;
-    return root.children.reduce((prev: number, curr: SyncableTree<EstimationNode>) => prev + this.mapWorkItemPackage(curr, riskFactors), 0);
-  }
 
-  private mapWorkItemPackage(pck: SyncableTree<EstimationNode>, factors: RiskFactors): number {
-    const items = pck.children as SyncableTree<EstimationLeaf>[];
-    return items.reduce((prev: number, curr: SyncableTree<EstimationLeaf>) => prev + this.mapItem(curr.data, factors), 0);
+    return calculateEffort(root);
   }
-
-  private mapItem(leaf: EstimationLeaf, factors: RiskFactors): number {
-    return +factors[leaf.risk] * leaf.effortInManDays;
-  }
-
 }
