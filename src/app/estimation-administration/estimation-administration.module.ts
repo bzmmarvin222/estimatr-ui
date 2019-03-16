bbada67bd4fe4b28d3a672beda50058811ea3086
bzmmarvin222 <bzmmarvin222@gmail.com>
2019-03-16 23:20:47 +0100

    added simple project overview

--- src/app/estimation-administration/estimation-administration.module.ts
+++ src/app/estimation-administration/estimation-administration.module.ts
@@ -4,12 +4,16 @@ import { OverviewComponent } from './overview/overview.component';
 import { OverviewItemComponent } from './overview/overview-item/overview-item.component';
 import {RouterModule} from '@angular/router';
 import {EstimationAdministraionRoutes} from './estimation-administration-routes';
+import {MatButtonModule, MatIconModule, MatListModule} from '@angular/material';
 
 @NgModule({
   declarations: [OverviewComponent, OverviewItemComponent],
   imports: [
     RouterModule.forChild(EstimationAdministraionRoutes),
-    CommonModule
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     OverviewComponent
--- src/app/estimation-administration/overview/overview-item/overview-item.component.ts
+++ src/app/estimation-administration/overview/overview-item/overview-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {EstimationDto} from '../../../estimatr-common/lib/estimation/dto/estimation.dto';
+import {EstimationRoot} from '../../../estimatr-common/lib/estimation/estimation';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'etmr-overview-item',
@@ -9,10 +11,15 @@ import {EstimationDto} from '../../../estimatr-common/lib/estimation/dto/estimat
 export class OverviewItemComponent implements OnInit {
 
   @Input() public estimation: EstimationDto;
+  public rootData: EstimationRoot;
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+    this.rootData = this.estimation.tree.data as EstimationRoot;
   }
 
+  public edit(): void {
+    this._router.navigate([`estimate/${this.estimation.id}`]);
+  }
 }
