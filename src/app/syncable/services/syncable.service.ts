65a0e919f8c9fb32647e54c78be1b5989de35410
bzmmarvin222 <bzmmarvin222@gmail.com>
2019-03-16 15:22:42 +0100

    fix auth header

--- src/app/syncable/services/syncable.service.ts
+++ src/app/syncable/services/syncable.service.ts
@@ -3,30 +3,18 @@ import {SyncableResource, SyncableTree, WebSocketHandler} from 'sync_ot';
 import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {EstimationNode} from '../../estimatr-common/lib/estimation/estimation';
-
-function getCookie(name: string): string {
-  const nameLenPlus = (name.length + 1);
-  return document.cookie
-    .split(';')
-    .map(c => c.trim())
-    .filter(cookie => {
-      return cookie.substring(0, nameLenPlus) === `${name}=`;
-    })
-    .map(cookie => {
-      return decodeURIComponent(cookie.substring(nameLenPlus));
-    })[0] || null;
-}
+import {AuthService} from '../../auth/services/auth.service';
 
 @Injectable()
 export class SyncableService {
   private _tree$;
   private _sr: SyncableResource<EstimationNode>;
 
-  constructor() {
+  constructor(private _auth: AuthService) {
   }
 
   public joinSession(estimationId: string): Observable<SyncableTree<EstimationNode>> {
-    const jwt = getCookie('jwt');
+    const jwt = this._auth.rawToken;
     const handler = new WebSocketHandler(environment.getWebsocketUrl(), estimationId, {Authorization: `Bearer ${jwt}`});
     this._sr = new SyncableResource(handler);
     this._tree$ = this._sr.getTree$();
